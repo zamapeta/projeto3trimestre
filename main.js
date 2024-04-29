62cabdb9b6425ac6a9b52d657d8e89f6a7a4fbc2
zamapeta <130806448+zamapeta@users.noreply.github.com>
2024-04-29 19:24:17 -0300

    c

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2020-10-05T23:59:59");
-const tempoObjetivo2 = new Date("2023-12-05T23:59:59");
-const tempoObjetivo3 = new Date("2023-12-30T23:59:59");
-const tempoObjetivo4 = new Date("2024-02-01T23:59:59");
+const tempoObjetivo1 = new Date("2025-10-05T23:59:59");
+const tempoObjetivo2 = new Date("2025-12-05T23:59:59");
+const tempoObjetivo3 = new Date("2025-12-30T23:59:59");
+const tempoObjetivo4 = new Date("2025-02-01T23:59:59");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
